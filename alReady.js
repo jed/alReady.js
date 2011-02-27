95cf7e8ad7c5ef22898b0d14ebb326ef9e1d5cab
Jed Schmidt <tr@nslator.jp>
2011-02-27 15:25:51 +0900

    first commit

---
+++ alReady.js
@@ -0,0 +1,16 @@
+alReady = function( n, t, ready ) {
+  // when ready, call any listeners, and then dereference the list
+  ready = function(){ for ( t = 0; ready && t < n; ) ready[ t++ ](); ready = 0 }
+  
+  // optimistically call addEventListener for modern browsers...
+  document.addEventListener && document.addEventListener( "domContentLoaded", ready, false )
+  
+  // ...but if readyState exists, poll it just in case with exponential backoff
+  document.readyState && function check() {
+    // try again if document.readyState contains `in`, otherwise call ready
+    ready && setTimeout( ~document.readyState.indexOf( "in" ) ? check : ready, t *= 2 )
+  }()
+
+  // call fn immediately if ready has fired, otherwise queue it
+  return function( fn ){ ready ? ready[ n++ ] = fn : fn() }
+}( 0, 1 )
