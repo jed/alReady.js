1a8eac936d318be9bbe5b2e955150a22541b60ae
Jed Schmidt <tr@nslator.jp>
2011-03-04 16:49:17 +0900

    removed polling in favor of native event binding
    with just a little more code, this covers both the worst case
    (window.onload) and the best case (DOMContentLoaded), with an
    additional check for "readystatechange" when document.readyState
    is "complete".

--- alReady.js
+++ alReady.js
@@ -1,29 +1,16 @@
-// trying a new tack, binding to events first
-// HEAVILY inspired by diego perini's awesome attempt here:
-// http://javascript.nwbox.com/ContentLoaded/
+alReady = function( fn ) {
+  var add = "addEventListener"
+    , win = this
+    , doc = win.document
+    , pre = doc[ add ] ? "" : "on"
 
-alReady = function() {
-  var events = [ "load", "DOMContentLoaded", "readystatechange" ]
-    , els = [ window, document, document ]
-    , i
-    , n = 0
-
-    , action = "addEventListener"
-    , prefix = ""
-    , w3c = document[ action ]
-
-    , ready = function( e ) {
-        if ( !ready || e.type == "readystatechange" && !/^c/.test( document.readyState ) ) return
-
-        for ( i = 3; i--; ) els[ i ][ action ]( prefix + events[ i ], ready, false )
-        for ( i = 0; i < n; ) ready[ i++ ]( e )
-        ready = 0
-      }
-        
-  if ( !w3c ) add = "attachEvent", prefix = "on"
-  
-  for ( i = 3; i--; ) els[ i ][ action ]( prefix + events[ i ], ready, false )
-  action = w3c ? "removeEventListener" : "detachEvent"
-  
-  return function( fn ){ ready ? ready[ n++ ] = fn : fn() }
-}()
+  "load DOMContentLoaded readystatechange".replace( /\w+/g, function( type, i ) {
+    ( i ? doc : win )
+      [ pre ? "attachEvent" : add ]
+      (
+        pre + type,
+        function(){ if ( fn ) if ( i < 6 || /m/( doc.readyState ) ) fn(), fn = 0 },
+        !1
+      )
+  })
+}
